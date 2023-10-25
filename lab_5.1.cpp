cf9ab15c2de15e3d1fecaf59bfdaafb8d9d00513
denysbandrivskyi <denys.bandrivskyi.ri.2023@lpnu.ua>
2023-10-26 01:48:23 +0300

    new commit

---
+++ lab_5.1.cpp
@@ -0,0 +1,17 @@
+﻿// Lab 5_1
+#include <iostream>
+#include <cmath>
+using namespace std;
+double k(const double x, const double y) // визначення
+{
+	return ((x / (1 + pow(sin(y), 2))) + (y / (1 + x * x)));
+}
+int main()
+{
+	double p, q;
+	cout << "p = "; cin >> p;
+	cout << "q = "; cin >> q;
+	double c = k((1 + (p * q)), (q * q)) + pow(k(p, p * p), 2);
+	cout << "c = " << c << endl;
+	return 0;
+}
